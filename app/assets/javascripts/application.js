b49bd97dc2ec9d3aadb519d60aba7525b838dc2c
Matthew Sampson <mbsampson89@gmail.com>
2018-09-10 15:57:25 -0700

    Add event listener to nav bar buttons and restyle nav-link buttons

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,6 +24,7 @@ console.log("Application.js loaded");
 
 $(document).ready(function() {
   setFooterClipboard();
+  setNavButtonListeners();
 });
 
 function setFooterClipboard() {
@@ -40,13 +41,23 @@ function setFooterClipboard() {
 
 function setNavButtonListeners() {
   const homeBtn = $('#home-btn');
-  const aboutBtn = $('#about-btn');
+  const experienceBtn = $('#experience-btn');
   const projectsBtn = $('#projects-btn');
   let homeContainer = $('#home-container');
-  let aboutContainer = $('#about-container');
+  let experienceContainer = $('#experience-container');
   let projectsContainer = $('#projects-container');
 
 
   // Show or hide containers depending on clicked button
+  experienceBtn.on('click', function() {
+    console.log('Handle exp click');
+    experienceContainer.removeClass('hidden');
+    projectsContainer.addClass('hidden');
+  });
 
+  projectsBtn.on('click', function() {
+    console.log('Handle proj click');
+    experienceContainer.addClass('hidden');
+    projectsContainer.removeClass('hidden');
+  });
 }
